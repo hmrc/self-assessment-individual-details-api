aa5bc0082e499b53243736a6a2ab70cbbe6b2b56
omotrium <omo4adex@gmail.com>
2025-12-30 10:49:54 +0000

    MTDSA-32437: Return 504 GATEWAY_TIMEOUT for 499s and 504s from downstream

--- app/shared/models/errors/CommonMtdErrors.scala
+++ app/shared/models/errors/CommonMtdErrors.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2025 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -51,6 +51,8 @@ object CalculationIdFormatError extends MtdError("FORMAT_CALCULATION_ID", "The p
 object NotFoundError extends MtdError("MATCHING_RESOURCE_NOT_FOUND", "Matching resource not found", NOT_FOUND)
 
 object InternalError extends MtdError("INTERNAL_SERVER_ERROR", "An internal server error occurred", INTERNAL_SERVER_ERROR)
+
+object GatewayTimeoutError extends MtdError("GATEWAY_TIMEOUT", "The request has timed out", GATEWAY_TIMEOUT)
 
 object BadRequestError extends MtdError("INVALID_REQUEST", "Invalid request", BAD_REQUEST)
 
