646fe95a4aa53469d93474eda447c33eff5e188d
Thepworth95 <56401451+Thepworth95@users.noreply.github.com>
2026-04-07 11:30:01 +0100

    New ES1 call for Auth

--- app/shared/services/EnrolmentsAuthService.scala
+++ app/shared/services/EnrolmentsAuthService.scala
@@ -55,7 +55,7 @@ class EnrolmentsAuthService @Inject() (val connector: AuthConnector,
 
   def authorised(mtdId: String, endpointAllowsSupportingAgents: Boolean = false)(implicit
       hc: HeaderCarrier,
-      ec: ExecutionContext): Future[AuthOutcome] =
+      ec: ExecutionContext): Future[AuthOutcome] = {
     authFunction
       .authorised(initialPredicate(mtdId))
       .retrieve(affinityGroup and authorisedEnrolments) {
@@ -74,18 +74,18 @@ class EnrolmentsAuthService @Inject() (val connector: AuthConnector,
                   .authorised(EnrolmentsAuthService.supportingAgentAuthPredicate(mtdId)) {
                     Future.successful(agentDetails(mtdId, authorisedEnrolments, "Supporting Agent"))
                   }
-//              case _: InsufficientEnrolments =>
-//                logger.warn(s"[EnrolmentsAuthService][authorised] Agent enrolment not found for MTDITID: $mtdId")
-//                Future.successful(Left(ClientNotEnrolledError))
+              case _: InsufficientEnrolments =>
+                logger.warn(s"[EnrolmentsAuthService][authorised] Agent enrolment not found for MTDITID: $mtdId")
+                Future.successful(Left(ClientNotEnrolledError))
             }
         case _ =>
           logger.warn(s"[EnrolmentsAuthService][authorised] Invalid AffinityGroup.")
           Future.successful(Left(ClientOrAgentNotAuthorisedError))
       }
       .recoverWith {
-//        case _: InsufficientEnrolments =>
-//          logger.warn(s"[EnrolmentsAuthService][authorised] Insufficient Enrolments")
-//          Future.successful(Left(ClientNotEnrolledError))
+        case _: InsufficientEnrolments =>
+          logger.warn(s"[EnrolmentsAuthService][authorised] Insufficient Enrolments")
+          Future.successful(Left(ClientNotEnrolledError))
         case _: AuthorisationException =>
           Future.successful(Left(ClientOrAgentNotAuthorisedError))
         case error =>
--- test/shared/services/EnrolmentsAuthServiceSpec.scala
+++ test/shared/services/EnrolmentsAuthServiceSpec.scala
@@ -17,11 +17,9 @@
 package shared.services
 
 import org.scalamock.handlers.CallHandler
-import play.api.Configuration
 import shared.config.{ConfidenceLevelConfig, MockSharedAppConfig}
-import shared.connectors.EnrolmentsAuthConnector
 import shared.models.auth.UserDetails
-import shared.models.errors.{ClientNotEnrolledError, ClientOrAgentNotAuthorisedError, InternalError, MtdError}
+import shared.models.errors.{ClientNotEnrolledError, ClientOrAgentNotAuthorisedError, InternalError}
 import shared.models.outcomes.AuthOutcome
 import shared.services.EnrolmentsAuthService.{
   authorisationDisabledPredicate,
@@ -236,7 +234,7 @@ class EnrolmentsAuthServiceSpec extends ServiceSpec with MockSharedAppConfig {
         mockConfidenceLevelCheckConfig(authValidationEnabled = authValidationEnabled)
 
         val result: AuthOutcome = await(enrolmentsAuthService.authorised(mtdId))
-        result shouldBe Left(ClientOrAgentNotAuthorisedError)
+        result shouldBe Left(ClientNotEnrolledError)
       }
 
     def disallowWhenNoBearerToken(authValidationEnabled: Boolean, initialPredicate: Predicate): Unit =
@@ -255,38 +253,11 @@ class EnrolmentsAuthServiceSpec extends ServiceSpec with MockSharedAppConfig {
     def disallowUsersWithoutEnrolments(authValidationEnabled: Boolean, initialPredicate: Predicate): Unit =
       "disallow users without enrolments" in new Test {
         mockConfidenceLevelCheckConfig(authValidationEnabled = authValidationEnabled)
-        MockedSharedAppConfig.featureSwitchConfig returns Configuration("ES1Call.enabled" -> false)
 
         MockedAuthConnector
           .authorised(initialPredicate, affinityGroup and authorisedEnrolments)
           .once()
           .returns(Future.failed(InsufficientEnrolments()))
-
-        MockedAuthConnector
-          .authorised(initialPredicate, allEnrolments)
-          .once()
-          .returns(Future.successful(Enrolments(Set.empty)))
-
-        MockedEnrolmentsAuthConnector
-          .getMtdId(mtdId)
-          .returns(Future.successful(ClientNotEnrolledError))
-
-        val result: AuthOutcome = await(enrolmentsAuthService.authorised(mtdId))
-        result shouldBe Left(ClientNotEnrolledError)
-      }
-
-      "disallow users without enrolments With ES! call" in new Test {
-        mockConfidenceLevelCheckConfig(authValidationEnabled = authValidationEnabled)
-        MockedSharedAppConfig.featureSwitchConfig returns Configuration("ES1Call.enabled" -> true)
-
-        MockedAuthConnector
-          .authorised(initialPredicate, affinityGroup and authorisedEnrolments)
-          .once()
-          .returns(Future.failed(InsufficientEnrolments()))
-
-        MockedEnrolmentsAuthConnector
-          .getMtdId(mtdId)
-          .returns(Future.successful(ClientNotEnrolledError))
 
         val result: AuthOutcome = await(enrolmentsAuthService.authorised(mtdId))
         result shouldBe Left(ClientNotEnrolledError)
@@ -294,10 +265,9 @@ class EnrolmentsAuthServiceSpec extends ServiceSpec with MockSharedAppConfig {
   }
 
   trait Test {
-    val mockAuthConnector: AuthConnector                     = mock[AuthConnector]
-    val mockEnrolmentsAuthConnector: EnrolmentsAuthConnector = mock[EnrolmentsAuthConnector]
+    val mockAuthConnector: AuthConnector = mock[AuthConnector]
 
-    lazy val enrolmentsAuthService = new EnrolmentsAuthService(mockAuthConnector, mockEnrolmentsAuthConnector, mockSharedAppConfig)
+    lazy val enrolmentsAuthService = new EnrolmentsAuthService(mockAuthConnector, mockSharedAppConfig)
 
     object MockedAuthConnector {
 
@@ -305,16 +275,6 @@ class EnrolmentsAuthServiceSpec extends ServiceSpec with MockSharedAppConfig {
         (mockAuthConnector
           .authorise[A](_: Predicate, _: Retrieval[A])(_: HeaderCarrier, _: ExecutionContext))
           .expects(predicate, retrievals, *, *)
-      }
-
-    }
-
-    object MockedEnrolmentsAuthConnector {
-
-      def getMtdId(mtdItId: String): CallHandler[Future[MtdError]] = {
-        (mockEnrolmentsAuthConnector
-          .getMtdIds(_: String)(_: HeaderCarrier, _: ExecutionContext))
-          .expects(mtdItId, *, *)
       }
 
     }
